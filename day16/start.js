b394b66a3e2ab1fcb676cef4b78b6c467b724ba4
Sutthinart Khunvadhana <sutthinart.khu@everapp.io>
2022-04-25 06:15:01 +0700

    feat: finish day 16

---
+++ day16/start.js
@@ -0,0 +1,23 @@
+(() => {
+  const message = new SpeechSynthesisUtterance();
+
+  function onVoicesChanged() {
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find(voice => voice.lang === 'th-TH');
+    message.voice = thVoice;
+  }
+
+  function onClick(event) {
+    message.text = event.target.getAttribute('alt');
+    speechSynthesis.speak(message);
+  }
+
+  function run() {
+    speechSynthesis.addEventListener('voiceschanged', onVoicesChanged);
+
+    const imgElems = Array.from(document.querySelectorAll('img'));
+    imgElems.forEach(imgElem => imgElem.addEventListener('click', onClick));
+  }
+
+  run();
+})();
